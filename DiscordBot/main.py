cef074feed1e1f7ddbec90e73b8cfcb380527bdd
Bastien Helec <bastienhelec.pro1@gmail.com>
2022-09-20 23:12:09 +0200

    2 commandes dans un fichiers !!

--- DiscordBot/main.py
+++ DiscordBot/main.py
@@ -1,11 +1,14 @@
 import asyncio
 import discord
 from discord.ext import commands
+from time import sleep
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='!',intents=intents)
+# bot=commands.Bot(command_prefix='!',intents=intents)
+
 
 @client.event
 async def on_ready():
@@ -18,5 +21,15 @@ async def on_message(message):
 
     if message.content.startswith('hello'):
         await message.channel.send('bonjour je suis un bot du groupe <@&1021394696754438145>')
+    if message.content.startswith('/clear'):
+        await message.channel.send('message en cours de suppression !')
+        sleep(0.5)
+        await message.channel.purge(limit=2)
+
+#↓ ceci est un test↓ 
+# @client.command()
+# async def clear(ctx, amount = 1):
+#     await ctx.channel.purge(limit=amount)
+#↑ ceci est un test↑
 
 client.run('NjIyMTI3NzkwMTkxNDExMjEw.G_AA5s.iIXkn7sRI5NVxC_2F2GlWdNjKGQb3whDOJp-Rw')
