a582c62b975d86fe2199eb6e924fc68d99b5c76f
Bastien-Helec <bastienhelec.pro1@gmail.com>
2022-09-21 15:13:02 +0200

    suite

--- DiscordBot/del.py
+++ DiscordBot/del.py
@@ -1,4 +1,5 @@
 import asyncio
+from pydoc import cli
 import discord
 from discord.ext import commands
 from time import sleep
@@ -16,30 +17,39 @@ async def on_ready():
 @client.command(name='clear', help='this command will clear msgs')
 async def clear(ctx, amount = 1):
     await ctx.channel.send(f'suppression de {amount} messages')
+    print(ctx)
     sleep(0.4)
     x=0
+    egal=["="]
     while x<=amount:
             x+=1
-            await ctx.channel.purge(limit=1)
             # await ctx.channel.send(' - '*x)
             if x>amount:
                 await ctx.channel.send('='*x+'>')
             else:
-                await ctx.channel.send('='*x)
+                egal=egal.append("="*x)
+                await ctx.channel.send(egal)
             sleep(0.2)
 
     await ctx.channel.purge(limit=amount)
 
 
-#ajouter un bloc de break pour plusieurs personnes qui repondent la meme chose 
-@client.event
-async def on_message(ctx):
-    if ctx.author == client.user:
-        return
+# @bot.command()
+# async def test(ctx, arg):
+#     await ctx.send(arg)
+#     print(ctx,arg)
 
-    if ctx.content.startswith('hello'):
-        await ctx.channel.send('bonjour je suis un bot du groupe <@&1021394696754438145>')
-    if ctx.content.startswith('nice to meet'):
-        await ctx.channel.send('nice to meet you too mister woman')
+#reponds au mention du bot uniquement
+# @client.event
+# async def on_message(ctx):
+    
+#     if ctx.author == client.user:
+#         return
+
+#     if ctx.content.startswith('hello'):
+#         await ctx.channel.send('bonjour je suis un bot du groupe <@&1021394696754438145>')
+#         print(ctx)
+#     if ctx.content.startswith('nice to meet'):
+#         await ctx.channel.send('nice to meet you too mister woman')
 
 client.run('NjIyMTI3NzkwMTkxNDExMjEw.G_AA5s.iIXkn7sRI5NVxC_2F2GlWdNjKGQb3whDOJp-Rw')
