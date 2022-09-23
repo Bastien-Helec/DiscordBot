ce697211d3ccf2659fb2e55e256316ae6fc457a0
Bastien Helec <bastienhelec.pro1@gmail.com>
2022-09-23 18:51:39 +0200

    ajout d'un fix pour accepter les commandes

--- DiscordBot/test2.py
+++ DiscordBot/test2.py
@@ -13,6 +13,17 @@ client = commands.Bot(command_prefix=PREFIX,intents=intents)
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello'):
+        await message.channel.send('bonjour je suis un bot du groupe <@&1021394696754438145>')
+    await client.process_commands(message)
+
+
 @client.command()
 async def clear(ctx, amount=2):
         if not amount : 
