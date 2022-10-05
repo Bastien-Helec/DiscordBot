1194d11a7d2b23a4ddebaba1d95102656105acdc
Bastien-Helec <bastienhelec.pro1@gmail.com>
2022-10-05 09:49:06 +0200

    ajout des legendes help

--- DiscordBot/test2.py
+++ DiscordBot/test2.py
@@ -37,6 +37,7 @@ async def on_message(message):
 
 @client.command()
 async def clear(ctx, amount=2):
+        '''Supprime les messages par defaut 2 pour plus d'info !help clear'''
         if not amount : 
             return await ctx.send("Erreur\nVous n'avez pas donner de valeur.")
         await ctx.channel.send(f'suppression de {amount} messages')
@@ -49,26 +50,32 @@ async def clear(ctx, amount=2):
 
 @client.command()
 async def hello(ctx):
+    '''Ceci est une commande de test'''
     await ctx.send('Hello')
 
 @client.command()
 async def deprime(ctx):
+    '''Tu deprimes ? tape cette commande elle va te faire sourrire'''
     await ctx.send('Il est temps de sourire !!! :)')
 
 @client.command()
 async def blowjob(ctx):
+    '''une commande nsfw'''
     await ctx.send('non pas ici -_-')
 
 @client.command()
 async def  RS(ctx):
+        '''Accueil de communauté'''
         await ctx.send('RS est une communauté complete prete a vous accueillir')
 
 @client.command()
 async def jmennui (ctx):
+    '''Toi aussi tu t'ennui , tape cette commande et une surprise va venir'''
     await ctx.send("ne t'inquietes pas moi aussi")
 
 @client.command()
 async def NSFW(ctx):
+    '''Commande global pour les NSFW commandes'''
     await ctx.send("Arrive prochainement !!")
     
 
